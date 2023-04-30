71a7ae616574297f570bd857dadf28461de36293
tmuramat <mt15hydrangea@gmail.com>
2023-05-01 00:59:11 +0900

    ADD:0501

--- CPP01/ex05/Harl.hpp
+++ CPP01/ex05/Harl.hpp
@@ -5,13 +5,13 @@ class Harl
 public:
 	Harl();
 	~Harl();
-	void Harl::complain(std::string level);
+	void complain(std::string level);
 
 private:
 	void debug(void);
 	void info(void);
 	void warning(void);
 	void error(void);
-	typedef void (* t_error_log)(void);
-	t_error_log _logs[4];
+	void (Harl::* _logs[4])(void);
+	std::string _levels[4];
 };
