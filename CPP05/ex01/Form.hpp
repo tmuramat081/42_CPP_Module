5f3877b9633e0b7cacf55e927526116df8e3b177
tmuramat <mt15hydrangea@gmail.com>
2023-05-27 00:36:10 +0900

    ADD: 0527

---
+++ CPP05/ex01/Form.hpp
@@ -0,0 +1,27 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <string>
+
+class Form
+{
+	public:
+		// Constructors
+		Form();
+		Form(const Form &copy);
+
+		// Destructor
+		~Form();
+
+		// Operators
+		Form & operator=(const Form &assign);
+
+	private:
+		const std::string _name;
+		bool sign;
+		int _grade;
+
+};
+
+#endif
