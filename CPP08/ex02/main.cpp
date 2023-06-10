034a1c1c2b8a5894c921c00a2c755256978c4174
tmuramat <mt15hydrangea@gmail.com>
2023-06-10 23:09:55 +0900

    ADD: 0610

--- CPP08/ex02/main.cpp
+++ CPP08/ex02/main.cpp
@@ -1,24 +1,36 @@
 #include "MutantStack.hpp"
 #include <list>
 
+#define BLUE "\033[0;94m"
+#define GREEN "\033[0;92m"
+#define YELLOW "\033[0;93m"
+#define RED "\033[0;91m"
+#define DEFAULT "\033[0;39m"
+
 void successful_test1()
 {
+	std::cout << GREEN << "-- Mutant stack --" << std::endl << DEFAULT;
 	MutantStack<int> mstack;
 
+	// push
 	mstack.push(5);
 	mstack.push(17);
 
+	// top
 	std::cout << mstack.top() << std::endl;
 
+	// pop
 	mstack.pop();
 
 	std::cout << mstack.size() << std::endl;
 
+	// push
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
 	mstack.push(0);
 
+	// iterator
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
 
@@ -29,29 +41,29 @@ void successful_test1()
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::cout << std::endl;
 }
 
 void successful_test2()
 {
-	std::list<int> mstack;
+	std::cout << GREEN << "-- List --" << std::endl << DEFAULT;
+	std::list<int> lstack;
 
-	mstack.push_back(5);
-	mstack.push_back(17);
+	lstack.push_back(5);
+	lstack.push_back(17);
 
-	std::cout << mstack.back() << std::endl;
+	std::cout << lstack.back() << std::endl;
 
-	mstack.pop_back();
+	lstack.pop_back();
 
-	std::cout << mstack.size() << std::endl;
+	std::cout << lstack.size() << std::endl;
 
-	mstack.push_back(3);
-	mstack.push_back(5);
-	mstack.push_back(737);
-	mstack.push_back(0);
+	lstack.push_back(3);
+	lstack.push_back(5);
+	lstack.push_back(737);
+	lstack.push_back(0);
 
-	std::list<int>::iterator it = mstack.begin();
-	std::list<int>::iterator ite = mstack.end();
+	std::list<int>::iterator it = lstack.begin();
+	std::list<int>::iterator ite = lstack.end();
 
 	++it;
 	--it;
@@ -60,7 +72,6 @@ void successful_test2()
 		std::cout << *it << std::endl;
 		++it;
 	}
-	std::cout << std::endl;
 }
 
 
